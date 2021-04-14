b8238f3f595de287c1f77900eca8139872c7c49c
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2021-04-14 13:26:49 +0530

    create bottle and ask

---
+++ org.winehq.flatpak-wine/resources/wine-create-bottle.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+# ============================================================================ #
+# This script will create a shortcut on applications menu for windows programs 
+# installed with wine.
+# Autor: Thiago silva
+# Contact: thiagos.dasilva@gmail.com
+# URL: https://github.com/thiggy01/wine-create-shortcut
+# Updated by Mohammed Asif Ali Rizvan for wine bottle
+# ============================================================================ #
+
+# Check if an application argument was submited.
+if [[ $(file --mime-type -b "$1") = application/* ]]; then
+
+    # Get the full path, the name and directory name of submited application file.
+    myFile=$(realpath "$1")
+    myBaseName=$(basename "$myFile")
+    myPath=$(dirname "$myFile")
+
+    # Function to verify if required program is installed.
+    VerInst () {
+	if [ $? -eq 127 ]; then
+	    echo "$1 package is not installed."
+	    echo "Install it before running this script."
+	    echo "Aborting."
+	    exit 1
+	fi
+    }
+
+    # Extract icon and convert it to several png files of diferent quality, 
+    wrestool -x -t 14 "$myFile" > "/tmp/$myBaseName.ico" 2> /dev/null
+    VerInst icoutils
+    convert -alpha on "/tmp/$myBaseName.ico" "/tmp/$myBaseName.png" 2> /dev/null
+    VerInst imagemagick
+    # Select the best png image file.
+    cp "$(ls -S -1 "/tmp/$myBaseName"*".png"  | head -n 1)" "$myFile.icon.png"
+
+    # Get the app category and shortcut name inputs from the user.
+    appCategory=$(zenity --title "wine-create-shortcut" --height=450 --list \
+ --radiolist --column " " --column "Categories" 0 AudioVideo 0 Audio 0 Video 0 \
+ Development 0 Education TRUE Game 0 Graphics 0 Network 0 Office 0 Settings 0 \
+ System 0 Utility  --text "Select a Category:" 2> /dev/null)
+
+
+    VerInst zenity
+    appName=$(zenity --title "wine-create-shortcut" --text "Enter a name for your \
+shortcut" --entry)
+
+    if [ ! $appName ]; #if no name is given use filename
+       then         
+       appName=$(echo "$myBaseName");     
+    fi
+
+# try to add wineprefix for the game
+mkdir -p ~/.wine-x86_64-bottles/
+
+myBaseNamePrefix=$(echo $myBaseName|tr ' ' '_');
+
+###############################################
+cat << EOF > ~/.wine-x86_64-bottles/$myBaseNamePrefix.sh
+
+choice=\$(zenity --title "$myBaseNamePrefix: Choose!" --width=240 --height=300 \
+                 --list \
+                 --radiolist --column " " \
+                 --column "Action" \
+                          0 "Install_DLLs" \
+                          TRUE "Launch $myBaseNamePrefix" \
+                          0 "Winetricks" \
+                          0 "Winecfg" \
+                          0 "Explore" \
+                          0 "Shell" \
+                          0 "Delete_Bottle" \
+                 --text "Select Action..." )
+
+
+[[ "\$?" != "0" ]] && exit 1
+
+if [ \$choice = "Winetricks" ]; then  
+   WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --gui
+elif [ \$choice = "Install_DLLs" ]; then
+(
+  echo 10
+  echo "# Installing xact..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended xact xact_x64
+
+  echo 20
+  echo "# Installing xinput..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended xinput
+
+  echo 40
+  echo "# Installing XNA31..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended xna31
+
+  echo 50
+  echo "# Installing vcrun 2005..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended vcrun2005 
+
+  echo 60
+  echo "# Installing vcrun 2008..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended vcrun2008
+
+  echo 70
+  echo "# Installing corefonts..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended vcrun2008
+
+  echo 80
+  echo "# Installing d3dx9..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended d3dx9
+
+  echo 90
+  echo "# Installing allcodecs..."
+  WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winetricks org.winehq.flatpak-wine --unattended allcodecs
+  
+  echo 100
+  echo "# Done!"
+) | zenity --title "Installing DLLs with Winetricks" --progress --auto-kill
+
+elif [ \$choice = "Winecfg" ]; then
+   WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=winecfg org.winehq.flatpak-wine
+elif [ \$choice = "Explore" ]; then
+   WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=wine org.winehq.flatpak-wine /app/explorer++/Explorer++.exe
+elif [ \$choice = "Shell" ]; then   
+ gnome-terminal -- bash -c "WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=bash org.winehq.flatpak-wine"
+elif [ \$choice = "Delete_Bottle" ]; then
+rm -rfv ~/.wine-x86_64-bottles/$myBaseNamePrefix; 
+rm -f "$HOME/.local/share/applications/wine-x86_64/$myBaseName.desktop"
+rm -f "$myFile.desktop" 
+rm -f "$myFile.icon.png"
+rm -f ~/.wine-x86_64-bottles/"$myBaseNamePrefix.sh"
+else
+WINEPREFIX=~/.wine-x86_64-bottles/$myBaseNamePrefix flatpak run --command=wine org.winehq.flatpak-wine '$myFile'
+fi
+EOF
+###############################################
+chmod +x ~/.wine-x86_64-bottles/"$myBaseNamePrefix.sh"
+chmod +x ~/.wine-x86_64-bottles/*.sh
+    
+    # Generate desktop entry specifications to be added to the application launcher.
+    deskEntry="[Desktop Entry]"\\n"Exec=bash -c ~/.wine-x86_64-bottles/$myBaseNamePrefix.sh"\\n"Name=\
+$appName"\\n"Path=$myPath"\\n"Type=Application"\\n"Categories=$appCategory;\
+$category;"\\n"Icon="$myFile.icon.png""
+    # Create a .desktop file and add desktop entries in it.
+    echo -e $deskEntry >"$myFile".desktop
+    echo "Keywords=flatpak; wine;" >> "$myFile".desktop
+    
+    # link the launcher  file to a shortcut on applications menu.
+    mkdir -p "$HOME/.local/share/applications/wine-x86_64/"
+    ln -s "$myFile.desktop" "$HOME/.local/share/applications/wine-x86_64/$myBaseName.desktop"
+    # Test if the app link was created sucessfully on applications menu 
+    if [ $? -eq 0 ]; then
+        gtk-update-icon-cache 
+        update-desktop-database ~/.local/share/applications 
+        update-desktop-database "$myPath"
+    	echo "Shortcut created sucessfuly on applications menu."
+    	zenity --info --title="Shortcut created sucessfuly " --text="$myBaseName.desktop"
+    fi
+
+else 
+
+# If no application was submited, show this message.
+	echo Please, submit a windows application file. 
+	echo Usage: ./wine-create-shortcut path/to/app.exe 
+	zenity --info --title="Select an EXE to create shortcut" --text="in Filemanager, right-click on an exe and select this!"
+
+fi
---
+++ org.winehq.flatpak-wine/resources/wine-x86_64.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+export WINEPREFIX=~/.wine-x86_64
+export WINEARCH=win64
+#export WINEDLLOVERRIDES="mscoree,mshtml="
+export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH:/app/lib:/app/lib32:/app/$NAME:/app/lib/wine:/app/lib32/wine:/app/lib/i386-linux-gnu:/app/lib/debug/lib/i386-linux-gnu
+
+export ARGV="$@"
+export WINEEXE="/app/bin/wine"
+export WINETRICKS="/app/bin/winetricks"
+
+
+# some games need to cd to the dir to work
+export base=$(basename $1)
+export dire=$(dirname $1)
+
+#echo $base >> ~/Documents/debug.txt
+#echo $dire >> ~/Documents/debug.txt
+# Create kill shortcut for convenience
+mkdir -p ~/.local/share/applications/wine-x86_64/
+echo '
+[Desktop Entry]
+Exec=flatpak kill org.winehq.flatpak-wine
+Name=flatpak-wine kill (5.0.4)
+Type=Application
+Categories=Application;;
+Icon=org.winehq.flatpak-wine-kill
+Keywords=flatpak; wine; kill;
+' > ~/.local/share/applications/wine-x86_64/killall_wine-5.0.4.desktop
+
+# if no argument passed then show a dialog with choices
+if [ $# -eq 0 ];  then
+   echo "No arguments supplied"
+   echo "launching explorer++"
+   
+   # for GUI Dialog
+   choice=$(zenity --title "flatpak-wine (5.0.4)" --width=240 --height=300 \
+					 --list \
+					 --radiolist --column " " \
+					 --column "Action" \
+							  0 "Winetricks" \
+							  0 "Install_DLLs" \
+							  0 "Winecfg" \
+						   TRUE "Explore" \
+							  0 "Delete_Bottle" \
+					 --text "Select Action for ~/.wine-x86_64 " )
+
+   # exit when Cancel is clicked
+   [[ "$?" != "0" ]] && exit 1
+   
+	if [ $choice = "Winetricks" ]; then  
+	   WINEPREFIX=~/.wine-x86_64 winetricks --gui
+
+	elif [ $choice = "Install_DLLs" ]; then
+	(
+	  echo 10
+	  echo "# Installing xact..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended xact xact_x64
+
+	  echo 20
+	  echo "# Installing xinput..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended xinput
+
+	  echo 40
+	  echo "# Installing XNA31..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended xna31
+
+	  echo 50
+	  echo "# Installing vcrun 2005..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended vcrun2005 
+
+	  echo 60
+	  echo "# Installing vcrun 2008..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended vcrun2008
+
+	  echo 70
+	  echo "# Installing corefonts..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended vcrun2008
+
+	  echo 80
+	  echo "# Installing d3dx9..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended d3dx9
+
+	  echo 90
+	  echo "# Installing allcodecs..."
+	  WINEPREFIX=~/.wine-x86_64 winetricks --unattended allcodecs
+	  
+	  echo 100
+	  echo "# Done!"
+	) | zenity --title "Installing DLLs with Winetricks" --progress --auto-kill
+
+	elif [ $choice = "Winecfg" ]; then
+	   WINEPREFIX=~/.wine-x86_64 winecfg
+
+	elif [ $choice = "Delete_Bottle" ]; then
+	   rm -rfv ~/.wine-x86_64; 
+       rm ~/.local/share/applications/wine-x86_64/killall_wine-5.0.4.desktop
+
+	elif [ $choice = "Explore" ]; then
+	   WINEPREFIX=~/.wine-x86_64 wine /app/explorer++/Explorer++.exe
+	   
+	else
+	   WINEPREFIX=~/.wine-x86_64 wine /app/explorer++/Explorer++.exe
+
+	fi
+    # /for GUI Dialog
+
+# For commandline
+elif   [ "$1" == "winecfg" ] ; then
+	/app/bin/winecfg
+	
+elif [ "$1" == "regedit" ] ; then
+	$WINEEXE regedit
+
+elif [ "$1" == "winetricks" ] ; then
+	ARGV=$(echo $ARGV|sed 's/winetricks//g')
+	$WINETRICKS $ARGV
+	
+elif [ "$1" == "bash" ] ; then
+	bash 
+	
+else
+#	$WINEEXE "$EXE" $@
+     cd $dire 2>/dev/null;#go to the exe directory then run
+     #echo $PWD - pwd >> ~/Documents/debug.txt
+     #echo now at $dire >> ~/Documents/debug.txt
+
+	$WINEEXE "$ARGV"
+	exit $?
+fi
