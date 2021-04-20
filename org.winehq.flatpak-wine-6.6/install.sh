1ed713ab9fa52f7e0c60b6964492d534357f877d
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2021-04-21 03:29:09 +0530

    wine 6.6

--- org.winehq.flatpak-wine-6.6/install.sh
+++ org.winehq.flatpak-wine-6.6/install.sh
@@ -6,9 +6,10 @@ sudo flatpak -y --system install org.freedesktop.Sdk/x86_64/20.08 \
                 org.freedesktop.Sdk.Compat.i386/x86_64/20.08 \
                 org.freedesktop.Sdk.Extension.toolchain-i386/x86_64/20.08 \
                 org.freedesktop.Platform.Compat.i386/x86_64/20.08 \
-                org.freedesktop.Platform.GL32.default/x86_64/20.08 \
+                org.freedesktop.Platform.GL.default/x86_64/20.08 \
 		org.freedesktop.Platform.VAAPI.Intel.i386/x86_64/20.08 \
-		org.freedesktop.Platform.VAAPI.Intel/x86_64/20.08
+		org.freedesktop.Platform.VAAPI.Intel/x86_64/20.08 \
+		org.freedesktop.Platform.ffmpeg-full/x86_64/20.08
 
 # If nvidia drivers are installed 
 if [ -x /usr/bin/nvidia-settings ]; then 
@@ -19,19 +20,21 @@ fi
 
 # Remove old flatpak of this, if installed
 rm *.flatpak repo/ -r 2>/dev/null
-#flatpak remove org.winehq.flatpak-wine -y 2>/dev/null
+#flatpak remove org.winehq.flatpak-wine66 -y 2>/dev/null
 
 
 # Build
-flatpak-builder --force-clean build-dir org.winehq.flatpak-wine.yml 
+flatpak-builder --force-clean build-dir org.winehq.flatpak-wine66.yml && \
+flatpak remove org.winehq.flatpak-wine66 -y && echo "removed previous version"
 
 
 # Create flatpak
-flatpak-builder --repo="repo" --force-clean build-dir/ org.winehq.flatpak-wine.yml 
-flatpak-builder --user --install --force-clean build-dir/ org.winehq.flatpak-wine.yml 
-flatpak --user remote-add --no-gpg-verify "org.winehq.flatpak-wine" "repo"
-#flatpak build-bundle "repo" "org.winehq.flatpak-wine_$date.flatpak" org.winehq.flatpak-wine stable --runtime-repo="https://flathub.org/repo/flathub.flatpakrepo"
-flatpak build-bundle "repo" "org.winehq.flatpak-wine.flatpak" org.winehq.flatpak-wine stable
+flatpak-builder --repo="repo" --force-clean build-dir/ org.winehq.flatpak-wine66.yml 
+
+flatpak-builder --user --install --force-clean build-dir/ org.winehq.flatpak-wine66.yml 
+flatpak --user remote-add --no-gpg-verify "org.winehq.flatpak-wine66" "repo"
+#flatpak build-bundle "repo" "org.winehq.flatpak-wine66_$date.flatpak" org.winehq.flatpak-wine66 stable --runtime-repo="https://flathub.org/repo/flathub.flatpakrepo"
+flatpak build-bundle "repo" "org.winehq.flatpak-wine66.flatpak" org.winehq.flatpak-wine66 stable
 
 # Install
-flatpak --user install org.winehq.flatpak-wine.flatpak -y
+flatpak --user install org.winehq.flatpak-wine66.flatpak -y
