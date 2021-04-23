d62a58e686541700fd27827036ed10081e10d8a6
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2021-04-24 01:30:01 +0530

    install dependencies

--- org.winehq.flatpak-wine/install.sh
+++ org.winehq.flatpak-wine/install.sh
@@ -4,21 +4,27 @@ if [ "$1" == "sdk" ]; then
 echo -n "Installing SDKs for build "
     sudo flatpak update -y
     sudo flatpak --system remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    sudo flatpak -y --system install org.freedesktop.Sdk/x86_64/20.08 \
+    sudo flatpak -y --system install flathub \
+	                org.freedesktop.Sdk/x86_64/20.08 \
                     org.freedesktop.Platform/x86_64/20.08 \
                     org.freedesktop.Sdk.Compat.i386/x86_64/20.08 \
                     org.freedesktop.Sdk.Extension.toolchain-i386/x86_64/20.08 \
                     org.freedesktop.Platform.Compat.i386/x86_64/20.08 \
                     org.freedesktop.Platform.GL32.default/x86_64/20.08 \
-		    org.freedesktop.Platform.VAAPI.Intel.i386/x86_64/20.08 \
-		    org.freedesktop.Platform.VAAPI.Intel/x86_64/20.08
+					org.freedesktop.Platform.GL.default/x86_64/20.08 \
+					org.freedesktop.Platform.VAAPI.Intel.i386/x86_64/20.08 \
+					org.freedesktop.Platform.VAAPI.Intel/x86_64/20.08 \
+					org.freedesktop.Platform.ffmpeg_full.i386/x86_64/20.08 \
+					org.freedesktop.Platform.ffmpeg-full/x86_64/20.08      
+					 
 
     # If nvidia drivers are installed 
-    if [ -x /usr/bin/nvidia-settings ]; then 
-    lsmod |grep -i nvidia && \
-    NVERSION=$(nvidia-settings -q all |grep OpenGLVersion|grep NVIDIA|sed 's/.*NVIDIA \(.*\) /nvidia-\1/g'|sed 's/\./-/g') && \
-    sudo flatpak install flathub org.freedesktop.Platform.GL32.$NVERSION/x86_64/20.08 org.freedesktop.Platform.GL32.$NVERSION/x86_64/1.4 -y   
-    fi                 
+	if [ -f /proc/driver/nvidia/version ]; then
+		ver=$(nvidia-settings -q all |grep OpenGLVersion|grep NVIDIA|sed 's/.*NVIDIA \(.*\) /nvidia-\1/g'|sed 's/\./-/g')
+		flatpak -y --system install flathub    \
+			org.freedesktop.Platform.GL.$ver   \
+			org.freedesktop.Platform.GL32.$ver
+	fi           
 
 fi 
 
@@ -43,7 +49,7 @@ flatpak-builder --repo="repo" --force-clean build-dir/ org.winehq.flatpak-wine.y
 echo "Installing flatpak-wine [x]"
 flatpak-builder --user --install --force-clean build-dir/ org.winehq.flatpak-wine.yml 
 echo "Creating flatpak bundle [x]"
-flatpak --user remote-add --no-gpg-verify "org.winehq.flatpak-wine" "repo"
+flatpak --user remote-add --no-gpg-verify "org.winehq.flatpak-wine" "repo" 2>/dev/null
 #flatpak build-bundle "repo" "org.winehq.flatpak-wine_$date.flatpak" org.winehq.flatpak-wine stable --runtime-repo="https://flathub.org/repo/flathub.flatpakrepo"
 flatpak build-bundle "repo" "org.winehq.flatpak-wine.flatpak" org.winehq.flatpak-wine stable
 
